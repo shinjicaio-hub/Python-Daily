dba96470c5c9614c6795a2940fafed3c8f5aef69
shinjicaio-hub <shinjicaiohub@outlook.com>
2026-02-11 20:47:31 -0300

    Atividade 1: Média de dois valores

---
+++ at001.py
@@ -0,0 +1,6 @@
+print('olá, mundo!')
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+med = ((nota1 + nota2)/2)
+print(f'A média das notas 1: {nota1}, e 2: {nota2} é de {med}.')
