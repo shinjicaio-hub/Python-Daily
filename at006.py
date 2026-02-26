c436dcbd0346a622b0bcffb9f5db27a649393ade
shinjicaio-hub <shinjicaiohub@outlook.com>
2026-02-25 21:11:18 -0300

    Atividade 6: Dobro, triplo e raiz

---
+++ at006.py
@@ -0,0 +1,7 @@
+valor = int(input('Digite um valor: '))
+
+dobro  = valor * 2
+triplo = valor * 3
+raiz   = (valor **(1/2))
+
+print(f'O dobro de {valor} é {dobro}.\nO triplo de {valor} é {triplo}.\nA raiz quadrada de {valor} tem o valor de {raiz:.2f}.')
