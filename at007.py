e71c7317e3e61cf91ce4cdb7fd62c659eafb5e48
shinjicaio-hub <shinjicaiohub@outlook.com>
2026-03-05 20:38:46 -0300

    Atividade 7: calculador media com 2 valores

---
+++ at007.py
@@ -0,0 +1,15 @@
+import math
+print('Média calculator')
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+med = [nota1, nota2]
+mf = (sum(med)/len(med))
+print(mf)
+
+#de forma aritmética:
+#nota1 = float(input('Digite a primeira nota: '))
+#nota2 = float(input('Digite a segunda nota: '))
+#med = ((nota1 + nota2)/2)
+#print(f'A média das notas 1: {nota1}, e 2: {nota2} é de {med}.')
+
+
