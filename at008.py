68624668f717d50a07ecc44c4e03167c463eb7a6
shinjicaio-hub <shinjicaiohub@outlook.com>
2026-03-09 20:10:35 -0300

    Atividade 8: Conversor de Medida

---
+++ at008.py
@@ -0,0 +1,6 @@
+print('Conversor de medida')
+metro = float(input('Digite o valor em metros: '))
+cm = metro * 100
+km = metro / 1000
+mm = metro * 1000
+print(f'O valor de {metro}mts em centímetros é: {cm}cm\nEm kilômetros: {km}km.\nEm milímetros: {mm}mm.')
