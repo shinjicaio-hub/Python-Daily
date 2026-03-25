77c93f4b26243cdbf754e06d4f78b14ed8ff8aa1
shinjicaio-hub <shinjicaiohub@outlook.com>
2026-03-25 19:50:11 -0300

    Atividade 16: Parte inteira de um valor

---
+++ at016.py
@@ -0,0 +1,11 @@
+import math
+valor = float(input('Digite um valor: '))
+print(f'O número {valor}, tem a parte inteira {math.trunc(valor)}.')
+
+# ou:
+
+valor = float(input('Digite um valor: '))
+print(f'O número {valor}, tem a parte inteira {int(valor)}')
+
+
+
